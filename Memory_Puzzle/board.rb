cdeddacd3d78669b97e5d7fbc28b0484aac6b9ad
Tristan Angeles <setswipe@gmail.com>
2021-08-24 13:32:56 -0400

    board updated(incomplete-on populate)

--- Memory_Puzzle/board.rb
+++ Memory_Puzzle/board.rb
@@ -4,12 +4,52 @@ class Board
 
     def initialize(n = 4)
         @card = Card.new
-        @board = Array.new(n){Array.new(n)}
+        @board = Array.new(n){Array.new(n) {:X}}
         @size = n * n
         @pairs = @size / 2
     end
 
+    def [] (position)
+      row , col = position
+      @board[row][col]
+
+    end
+
+    def []= (position, value)
+      row , col = position
+      @board[row][col] = value
+
+    end
+
     def populate
+
+      count = 0
+
+      while (count<@pairs)
+        row = (0...@board.length).sample  #gets initial position, may/maynot be taken
+        col = (0...@board.length).sample
+        position = [row, col]
+        card_count = 0
+        card = 'A' #Card.new()
+
+        while card_count<=2
+
+          while self[positon] != :X       #rolls until position is not taken
+            row = (0...@board.length).sample
+            col = (0...@board.length).sample
+            position = [row, col]
+          end   
+
+          self[positon] = card   # places first pair
+          card_count+=1
+        end
+
+   
+
+
+
+      end
+        
         #make @pairs num of cards
         #puts the cards in the board randomly
         #do it again until there are 2 of each card
